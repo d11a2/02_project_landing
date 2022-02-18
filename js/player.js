f3b3b17d5d6756bc76591a48d8dde3514cafd0ac
d11a2 <95178215+d11a2@users.noreply.github.com>
2022-02-18 08:11:40 +0300

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,113 @@
+let player;
+const playerContainer = $(".player");
+
+let eventsInit = () => {
+    $(".player__start").click(e => {
+        e.preventDefault();
+
+        if (playerContainer.hasClass("paused")) {
+            //playerContainer.removeClass("paused");
+            player.pauseVideo();
+        } else {
+            //playerContainer.addClass("paused");
+            player.playVideo();
+        }
+    });
+};
+
+$(".player__playback").click( e => {
+    const bar = $(e.currentTarget);
+    const clickedPosition = e.originalEvent.layerX;
+    const newCirclePositionPercent = clickedPosition / bar.width() * 100;
+    const newPlaybackPositionSecond = 
+    (player.getDuration() / 100) * newCirclePositionPercent;
+
+
+    $(".player__playback-slider .inner-circle").css({
+        left: `${newCirclePositionPercent}%`
+    });
+
+    player.seekTo(newPlaybackPositionSecond);
+
+});
+
+$(".player__splash").click( e => {
+    player.playVideo();
+});
+
+const formatTime = timeSec => {
+    const roundTime = Math.round(timeSec);
+
+    const minutes = addZero(Math.floor(roundTime / 60));
+    const seconds = addZero(roundTime - minutes * 60);
+    
+    function addZero(num) {
+        return num < 10 ? `0${num}` : num;
+    }
+
+    return `${minutes} : ${seconds}`;
+};
+
+const onPlayerReady = () => {
+    let interval;
+    const durationSec = player.getDuration();
+
+    $(".player__duration__estimate").text(formatTime(durationSec));
+
+    if (typeof interval !== "undefined") {
+        clearInterval(interval);
+    }
+
+    interval = setInterval(() => {
+
+        const completedSec = player.getCurrentTime();
+        const completedPercent = (completedSec / durationSec) * 100;
+
+        $(".player__playback-slider .inner-circle").css({
+            left: `${completedPercent}%`
+        });
+
+        $(".player__playback-slider .player__playback__progress-line").css({
+            width: `${completedPercent}%`
+        });
+
+        $(".player__duration__completed").text(formatTime(completedSec));
+    } , 1000);
+};
+
+const onPlayerStateChange = event => {
+
+    switch (event.data) {
+        case 1:
+            playerContainer.addClass("active");
+            playerContainer.addClass("paused");
+            break;
+        case 2:
+            playerContainer.removeClass("active");
+            playerContainer.removeClass("paused");
+            break;
+    }
+};
+
+function onYouTubeIframeAPIReady() {
+   
+    player = new YT.Player("yt-player", {
+        height: '392',
+        width: '662',
+        videoId: '7gphiFVVtUI',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        },
+        playerVars: {
+            controls: 0,
+            disablekb: 0,
+            showinfo: 0,
+            rel: 0,
+            autoplay: 0,
+            modestbranding: 0
+        }
+    });
+  }
+
+  eventsInit();
